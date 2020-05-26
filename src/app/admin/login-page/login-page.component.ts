ab34996569aaf401230db49ce4b7dee49924eef8
skuratoff <ya-skuratoff@yandex.by>
2020-05-26 22:52:47 +0300

    protection routes

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { User } from 'src/app/shared/interfaces';
 import { AuthService } from '../shared/services/auth.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -13,13 +13,22 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup
   submitted = false
+  message: string
 
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['loginAgain']) {
+        this.message = 'Please enter the data!'
+}
+    })
+
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
