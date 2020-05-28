47f236b1af5140e8bcd17c79b896aace4174a8ae
skuratoff <ya-skuratoff@yandex.by>
2020-05-28 17:49:16 +0300

    work with posts (list output, filtering, deletion)

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,16 +1,16 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
-import {LoginPageComponent} from './login-page/login-page.component';
-import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
-import {CreatePageComponent} from './create-page/create-page.component';
-import {EditPageComponent} from './edit-page/edit-page.component';
-import { AuthService } from './shared/services/auth.service';
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthGuard } from './shared/services/auth.guard';
+import { SearchPipe } from './shared/search.pipe';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { AuthGuard } from './shared/services/auth.guard';
     LoginPageComponent,
     DashboardPageComponent,
     CreatePageComponent,
-    EditPageComponent
+    EditPageComponent,
+    SearchPipe
   ],
   imports: [
     CommonModule,
@@ -28,17 +29,17 @@ import { AuthGuard } from './shared/services/auth.guard';
     RouterModule.forChild([
       {
         path: '', component: AdminLayoutComponent, children: [
-          {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
-          {path: 'login', component: LoginPageComponent},
-          {path: 'dashboard', component: DashboardPageComponent, canActivate:[AuthGuard]},
-          {path: 'create', component: CreatePageComponent, canActivate:[AuthGuard]},
-          {path: 'post/:id/edit', component: EditPageComponent, canActivate:[AuthGuard]}
+          { path: '', redirectTo: '/admin/login', pathMatch: 'full' },
+          { path: 'login', component: LoginPageComponent },
+          { path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard] },
+          { path: 'create', component: CreatePageComponent, canActivate: [AuthGuard] },
+          { path: 'post/:id/edit', component: EditPageComponent, canActivate: [AuthGuard] }
         ]
       }
     ])
   ],
   exports: [RouterModule],
-  providers:[AuthGuard]
+  providers: [AuthGuard]
 })
 export class AdminModule {
 
--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -10,12 +10,29 @@ export class PostService {
     constructor(private http: HttpClient) { }
     create(post: Post): Observable<Post> {
         return this.http.post(`${environment.fbDbUrl}/posts.json`, post)
-        .pipe(map((responce:FbCreateResponce)=>{
-            return {
-                ...post,
-                id:responce.name,
-                date:new Date(post.date)
-            }
-        }))
+            .pipe(map((responce: FbCreateResponce) => {
+                return {
+                    ...post,
+                    id: responce.name,
+                    date: new Date(post.date)
+                }
+            }))
+    }
+
+    getAll() {
+        return this.http.get(`${environment.fbDbUrl}/posts.json`)
+            .pipe(map((responce: { [key: string]: any }) => {
+                return Object
+                    .keys(responce)
+                    .map(key => ({
+                        ...responce[key],
+                        id: key,
+                        date: new Date(responce[key].date)
+                    }))
+            }))
+    }
+
+    remove(id: string): Observable<void> {
+        return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`)
     }
 }
