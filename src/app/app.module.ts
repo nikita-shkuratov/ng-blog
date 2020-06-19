e9bfda8a0766b49e1c4ba7c2b55ba4e34924dfa7
skuratoff <ya-skuratoff@yandex.by>
2020-06-19 23:54:19 +0300

    adding PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Provider } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { PostComponent } from './shared/components/post/post.component';
 import { SharedModule } from './admin/shared/shared.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './shared/auth.interceptor';
+import { environment } from '../environments/environment';
 
 
 const INTERCEPTOR_PROVIDER:Provider = {
@@ -29,7 +31,8 @@ const INTERCEPTOR_PROVIDER:Provider = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
